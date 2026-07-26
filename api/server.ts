373bc245d057c1a890613e31bc6dce5fae56c2b6
campusai071-wq <campusai071@gmail.com>
2026-07-26 19:12:18 +0100

    feat: expand dynamic sitemap with school calculators
    Include a predefined list of university aggregate calculators in the dynamic sitemap generation to improve SEO and discoverability for key institutions.

--- api/server.ts
+++ api/server.ts
@@ -221,6 +221,10 @@ app.get("/sitemap.xml", async (req: any, res: any) => {
       "/cookies"
     ];
 
+    const schoolSlugs = [
+      "unilag", "ui", "oau", "lasu", "uniben", "futa", "unn", "abu-zaria", "unilorin", "buk", "uniport", "uniuyo", "fuoye", "delsu", "kwasu"
+    ];
+
     let newsSlugs: string[] = [];
     try {
       const snapshot = await db.collection('news').where('isLive', '==', true).get();
@@ -235,12 +239,18 @@ app.get("/sitemap.xml", async (req: any, res: any) => {
     }
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
   ${staticPages.map(page => `
   <url>
     <loc>${baseUrl}${page}</loc>
     <changefreq>${page === "" || page === "/news" ? "daily" : "monthly"}</changefreq>
     <priority>${page === "" ? "1.0" : "0.8"}</priority>
+  </url>`).join('')}
+  ${schoolSlugs.map(slug => `
+  <url>
+    <loc>${baseUrl}/${slug}-aggregate-calculator</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.85</priority>
   </url>`).join('')}
   ${newsSlugs.map(slug => `
   <url>
